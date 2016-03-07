0297ae275d400f121f912778a9b0b5e507f04171
Adrien Menella <adrien.menella@gmail.com>
2016-03-07 22:03:10 +0100

    debut getNext Lexer.cpp

--- src/Lexer.cpp
+++ src/Lexer.cpp
@@ -10,8 +10,9 @@
 //---------------------------------------------------------------- INCLUDE
 
 //-------------------------------------------------------- Include système
-using namespace std;
+#include <string>
 #include <iostream>
+using namespace std;
 
 //------------------------------------------------------ Include personnel
 #include "Lexer.h"
@@ -31,6 +32,87 @@ Symbole Lexer::getNext ()
 // Algorithme :
 //
 {
+	char charCourant = '';
+	string symboleCourant = "";
+
+	for(i=0; this->programmeEnLecture.length(); i++)
+	{
+		charCourant = this->programmeEnLecture[i];
+
+		cout << "charCourant = " << charCourant << endl;
+
+		symboleCourant += charCourant;
+
+		cout << "symboleCourant = " << symboleCourant << endl;
+
+
+
+		if(charCourant==' '
+			|| charCourant==','
+			|| charCourant==';'
+			|| charCourant=='+'
+			|| charCourant=='-'
+			|| charCourant=='*'
+			|| charCourant=='/'
+			|| charCourant=='='
+			|| charCourant=='('
+			|| charCourant==')') {
+
+			switch(symboleCourant)
+			{
+				case 'var' :
+					Automate::transition(e_symbole::Var);
+					break;
+				case 'const' :
+					Automate::transition(e_symbole::Const);
+					break;
+				case 'ecrire' :
+					Automate::transition(e_symbole::Ecrire);
+					break;
+				case 'lire' :
+					Automate::transition(e_symbole::Lire);
+					break;
+				case ',' :
+					Automate::transition(e_symbole::Virg);
+					break;
+				case ';' :
+					Automate::transition(e_symbole::Pv);
+					break;
+				case '+' :
+					Automate::transition(e_symbole::Add);
+					break;
+				case '-' :
+					Automate::transition(e_symbole::Sub);
+					break;
+				case '*' :
+					Automate::transition(e_symbole::Mul);
+					break;
+				case '/' :
+					Automate::transition(e_symbole::Div);
+					break;
+				case ':=' :
+					Automate::transition(e_symbole::Aff);
+					break;
+				case '=' :
+					Automate::transition(e_symbole::Eg);
+					break;
+				case '(' :
+					Automate::transition(e_symbole::Po);
+					break;
+				case ')' :
+					Automate::transition(e_symbole::Pf);
+					break;
+				default :
+					Automate::transition(e_symbole::Id);
+					break;
+			}
+
+			symboleCourant = "";
+
+		}
+		
+	}
+
 } //----- Fin de Méthode getNext()
 
 
@@ -53,13 +135,15 @@ Lexer::Lexer ( const Lexer & unLexer )
 } //----- Fin de Lexer (constructeur de copie)
 
 
-Lexer::Lexer ( )
+Lexer::Lexer ( string programme )
 // Algorithme :
 //
 {
 #ifdef MAP
     cout << "Appel au constructeur de <Lexer>" << endl;
 #endif
+
+    this->programmeEnLecture = programme;
 } //----- Fin de Lexer
 
 
