a7cf3b154b7167f22b59a9c28fc94ba94edbd6ca
emilienbai <emilien.bai@insa-lyon.fr>
2016-03-07 10:00:14 +0100

    Clean des Classes états - Avec squelette

--- src/Etat/Etat.cpp
+++ src/Etat/Etat.cpp
@@ -1,10 +1,74 @@
+/*************************************************************************
+                           Etat  -  description
+                             -------------------
+    début                : 7 mars 2016
+    copyright            : (C) 2016 par E.
+*************************************************************************/
+
+//---------- Réalisation de la classe <Etat> (fichier Etat.cpp) --
+
+//---------------------------------------------------------------- INCLUDE
+
+//-------------------------------------------------------- Include système
+using namespace std;
 #include <iostream>
+
+//------------------------------------------------------ Include personnel
 #include "Etat.h"
 
-using  namespace std;
+//------------------------------------------------------------- Constantes
 
-class Etat {
-public:
+//---------------------------------------------------- Variables de classe
 
-	Etat () {}
-};
+//----------------------------------------------------------- Types privés
+
+
+//----------------------------------------------------------------- PUBLIC
+//-------------------------------------------------------- Fonctions amies
+
+//----------------------------------------------------- Méthodes publiques
+
+//------------------------------------------------- Surcharge d'opérateurs
+Etat & Etat::operator = ( const Etat & unEtat )
+// Algorithme :
+//
+{
+} //----- Fin de operator =
+
+
+//-------------------------------------------- Constructeurs - destructeur
+Etat::Etat ( const Etat & unEtat )
+// Algorithme :
+//
+{
+#ifdef MAP
+    cout << "Appel au constructeur de copie de <Etat>" << endl;
+#endif
+} //----- Fin de Etat (constructeur de copie)
+
+
+Etat::Etat ( )
+// Algorithme :
+//
+{
+#ifdef MAP
+    cout << "Appel au constructeur de <Etat>" << endl;
+#endif
+} //----- Fin de Etat
+
+
+Etat::~Etat ( )
+// Algorithme :
+//
+{
+#ifdef MAP
+    cout << "Appel au destructeur de <Etat>" << endl;
+#endif
+} //----- Fin de ~Etat
+
+
+//------------------------------------------------------------------ PRIVE
+
+//----------------------------------------------------- Méthodes protégées
+
+//------------------------------------------------------- Méthodes privées
--- src/Etat/Etat.h
+++ src/Etat/Etat.h
@@ -1,17 +1,94 @@
+/*************************************************************************
+                           Etat  -  description
+                             -------------------
+    début                : 07/03/2016
+    copyright            : (C) 2016 par E. Bai
+*************************************************************************/
+
+//---------- Interface de la classe <Etat> (fichier Etat.h) ------
 #if ! defined ( ETAT_H )
 #define ETAT_H
 
+//--------------------------------------------------- Interfaces utilisées
 #include "Automate.h"
 #include "Symbole.h"
 
+//------------------------------------------------------------- Constantes 
+
+//------------------------------------------------------------------ Types 
+
+//------------------------------------------------------------------------ 
+// Rôle de la classe <Etat>
+//
+//
+//------------------------------------------------------------------------ 
 
 class Etat
 {
+//----------------------------------------------------------------- PUBLIC
 
 public:
-    Etat();
+//----------------------------------------------------- Méthodes publiques
+    // type Méthode ( liste de paramètres );
+    // Mode d'emploi :
+    //
+    // Contrat :
+    //
 
-    virtual Etat transition(Automate automate, Symbole symbole);
+    virtual Etat transition (Automate automate, Symbole symbole);
+    // Mode d'emploi :
+    //
+    // Contrat :
+    //
+
+    //------------------------------------------------- Surcharge d'opérateurs
+    Lexer & operator = ( const Etat & unEtat);
+    // Mode d'emploi :
+    //
+    // Contrat :
+    //
+
+//-------------------------------------------- Constructeurs - destructeur
+    Etat ( const Etat & unEtat );
+    // Mode d'emploi (constructeur de copie) :
+    //
+    // Contrat :
+    //
+
+    Etat ( );
+    // Mode d'emploi :
+    //
+    // Contrat :
+    //
+
+    virtual ~Etat ( );
+    // Mode d'emploi :
+    //
+    // Contrat :
+    //
+
+//------------------------------------------------------------------ PRIVE 
+
+protected:
+//----------------------------------------------------- Méthodes protégées
+
+private:
+//------------------------------------------------------- Méthodes privées
+
+protected:
+//----------------------------------------------------- Attributs protégés
+
+private:
+//------------------------------------------------------- Attributs privés
+
+//---------------------------------------------------------- Classes amies
+
+//-------------------------------------------------------- Classes privées
+
+//----------------------------------------------------------- Types privés
+
 };
 
-#endif 
+//----------------------------------------- Types dépendants de <Lexer>
+
+#endif // ETAT_H
