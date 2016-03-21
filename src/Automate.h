99f06079cade7d1095be8687cb090abdfef81cda
MrBerts <berthier.gllm@gmail.com>
2016-03-21 19:23:08 +0100

    Some clean

--- src/Automate.h
+++ src/Automate.h
@@ -9,109 +9,36 @@
 #if ! defined ( AUTOMATE_H )
 #define AUTOMATE_H
 
-//--------------------------------------------------- Interfaces utilisées
 #include <stack>
 #include "Lexer.h"
 #include "Etat/Etat.h"
 
-//------------------------------------------------------------- Constantes
-
-//------------------------------------------------------------------ Types
-
-//------------------------------------------------------------------------
-// Rôle de la classe <Automate>
-//
-//
-//------------------------------------------------------------------------
-
 class Automate
 {
 //----------------------------------------------------------------- PUBLIC
-
 public:
-//----------------------------------------------------- Méthodes publiques
-    void lecture();
-    // Mode d'emploi :
-    //
-    // Contrat :
-    //
-
-    void pushState(Etat * etat);
-    // Mode d'emploi :
-    //
-    // Contrat :
-    //
-
-    void popState();
-    // Mode d'emploi :
-    //
-    // Contrat :
-    //
-
-    void transition(Symbole symbole);
-    // Mode d'emploi :
-    //
-    // Contrat :
-    //
-    
-    void accepte();
-    // Mode d'emploi :
-    //
-    // Contrat :
-    //
-    
-    void rejette();
-    // Mode d'emploi :
-    //
-    // Contrat :
-    //
-
-//------------------------------------------------- Surcharge d'opérateurs
-//    Automate & operator = ( const Automate & unAutomate );
-    // Mode d'emploi :
-    //
-    // Contrat :
-    //
-
-
-//-------------------------------------------- Constructeurs - destructeur
     Automate (const string & prog, bool affichage, bool analyseStatique, bool execution, bool transformation);
-    // Mode d'emploi :
-    //
-    // Contrat :
-    //
 
     virtual ~Automate ( );
-    // Mode d'emploi :
-    //
-    // Contrat :
-    //
+
+    void lecture();
+
+    void pushState(Etat * etat);
+
+    void popState();
+
+    void accepte();
+
+    void rejette();
+
+    void analyseStatique();
 
 //------------------------------------------------------------------ PRIVE
-
-protected:
-//----------------------------------------------------- Méthodes protégées
-
 private:
-//------------------------------------------------------- Méthodes privées
-
-protected:
-//----------------------------------------------------- Attributs protégés
-
-private:
-//------------------------------------------------------- Attributs privés
 	Lexer * lexer;
 	stack<Etat> pileEtats;
 	stack<Symbole> pileSymboles;
 
-//---------------------------------------------------------- Classes amies
-
-//-------------------------------------------------------- Classes privées
-
-//----------------------------------------------------------- Types privés
-
 };
-
-//----------------------------------------- Types dépendants de <Automate>
 
 #endif // AUTOMATE_H
--- src/Vids.cpp
+++ src/Vids.cpp
@@ -6,64 +6,35 @@
 *************************************************************************/
 
 //---------- Réalisation de la classe <Vids> (fichier Vids.cpp) --
-
-//---------------------------------------------------------------- INCLUDE
-
-//-------------------------------------------------------- Include système
 using namespace std;
 #include <iostream>
 
-//------------------------------------------------------ Include personnel
 #include "Vids.h"
 
-//------------------------------------------------------------- Constantes
-
-//---------------------------------------------------- Variables de classe
-
-//----------------------------------------------------------- Types privés
-
-
-//----------------------------------------------------------------- PUBLIC
-//-------------------------------------------------------- Fonctions amies
-
-//----------------------------------------------------- Méthodes publiques
-
-
-//------------------------------------------------- Surcharge d'opérateurs
-
-
-
-//-------------------------------------------- Constructeurs - destructeur
+//------------------------------------------------------------------ PUBLIC
 
 Vids::Vids ( )
-// Algorithme :
-//
 {
 #ifdef MAP
     cout << "Appel au constructeur de <Vids>" << endl;
 #endif
 } //----- Fin de Vids
 
-
 Vids::~Vids ( )
-// Algorithme :
-//
 {
 #ifdef MAP
     cout << "Appel au destructeur de <Vids>" << endl;
 #endif
+
 	Vids::mapVid.clear();
 } //----- Fin de ~Vids
 
-
 //------------------------------------------------------------------ PRIVE
 
-//----------------------------------------------------- Méthodes protégées
-void addVid(Id aId) {
+void Vids::addVid(Id aId) {
 	Vids::mapVid.insert(pair<Id, Val>(aId, (Val)(NULL)));
 }
 
-void affecter(Id aId, Val aVal) {
+void Vids::affecter(Id aId, Val aVal) {
 	Vids::mapVid[aId] = aVal;
 }
-//------------------------------------------------------- Méthodes privées
