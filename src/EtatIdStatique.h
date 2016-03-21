3dbf309384f5cee9b5d360037876a7aaaa595331
MrBerts <berthier.gllm@gmail.com>
2016-03-21 23:48:19 +0100

    Oops, actually, it was AnalyseStatique class

---
+++ src/EtatIdStatique.h
@@ -0,0 +1,29 @@
+/*************************************************************************
+                           EtatIdStatique  -  description
+                             -------------------
+    début                : 21 mars 2016
+    copyright            : (C) 2016 par G. Berthier
+*************************************************************************/
+
+//---------- Interface de la classe <EtatIdStatique> (fichier EtatIdStatique.h) ------
+#if ! defined ( ETATIDSTATIQUE_H )
+#define ETATIDSTATIQUE_H
+
+class EtatIdStatique
+{
+//----------------------------------------------------------------- PUBLIC
+public:
+    EtatIdStatique();
+    virtual ~EtatIdStatique();
+
+//------------------------------------------------------------------ PRIVE
+private:
+    bool constante;
+    bool variable;
+    bool declare;
+    bool affecte;
+    bool utilise;
+
+};
+
+#endif // ETATIDSTATIQUE_H
