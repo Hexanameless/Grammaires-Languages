65a71adc41af3a41f9c7b24dbf925f7464a40379
Adrien Menella <adrien.menella@gmail.com>
2016-03-24 08:54:20 +0100

    modification cout to cerr ExpDiv::operation

--- src/ExpDiv.cpp
+++ src/ExpDiv.cpp
@@ -7,7 +7,7 @@ ExpDiv::ExpDiv(Exp * g, Exp * d) : ExpBin(g,d) { }
 ExpDiv::~ExpDiv() { }
 
 double ExpDiv::operation(double g, double d) {
-	if(d==0) std::cout << "Division par zero impossible" << endl;
+	if(d==0) std::cerr << "Division par zero impossible" << endl;
    return g/d;
 }
 
