dd36651707f4171a911714b2ac1cfc4c0b99745f
MrBerts <berthier.gllm@gmail.com>
2016-03-28 22:11:36 +0200

    ExpDiv::afficher()

--- src/ExpDiv.cpp
+++ src/ExpDiv.cpp
@@ -3,42 +3,49 @@
 #include "ExpDiv.h"
 using namespace std;
 
-	ExpDiv::ExpDiv(Exp * g, Exp * d) : ExpBin(g,d) { idSymbole = T; } 
+ExpDiv::ExpDiv(Exp * g, Exp * d) : ExpBin(g,d) { idSymbole = T; }
 
-	ExpDiv::~ExpDiv() { }
+ExpDiv::~ExpDiv() { }
 
-	double ExpDiv::evaluation(const std::map<Id*,Exp*> & variables)
+double ExpDiv::evaluation(const std::map<Id*,Exp*> & variables)
+{
+	double d = droite->evaluation(variables);
+
+	if(d==0)
 	{
-		double d = droite->evaluation(variables);
-
-		if(d==0) 
-		{
-			// en cas de division par 0 on renvoie un message d'erreur et arrête l'éxecution
-			std::cerr << "Division par zero impossible" << endl;
-			exit(EXIT_FAILURE);
-		}
-
-		return (gauche->evaluation(variables) / d);
+		// en cas de division par 0 on renvoie un message d'erreur et arrête l'éxecution
+		std::cerr << "Division par zero impossible" << endl;
+		exit(EXIT_FAILURE);
 	}
 
-	Exp* ExpDiv::optimisation() {
-		Exp* expGauche = gauche->optimisation();
-		Exp* expDroite = droite->optimisation();
+	return (gauche->evaluation(variables) / d);
+}
 
-		if(expGauche->getId()==VAL && expDroite->getId()==VAL)
+Exp* ExpDiv::optimisation() {
+	Exp* expGauche = gauche->optimisation();
+	Exp* expDroite = droite->optimisation();
+
+	if(expGauche->getId()==VAL && expDroite->getId()==VAL)
+	{
+		if ( dynamic_cast <Val*>(expDroite)->getValeur()!=0 )
 		{
-			if ( dynamic_cast <Val*>(expDroite)->getValeur()!=0 )
-			{
-				delete this;
-				return new Val(dynamic_cast <Val*>(expDroite)->getValeur() / dynamic_cast <Val*>(expDroite)->getValeur());
-			}
-		} else if (expGauche->getId()==ID && expDroite->getId()==VAL)
-		{
-			if (dynamic_cast <Val*>(expDroite)->getValeur()==1)
-			{
-				delete this;
-				return expGauche;
-			}
+			delete this;
+			return new Val(dynamic_cast <Val*>(expDroite)->getValeur() / dynamic_cast <Val*>(expDroite)->getValeur());
 		}
-		return this;
+	} else if (expGauche->getId()==ID && expDroite->getId()==VAL)
+	{
+		if (dynamic_cast <Val*>(expDroite)->getValeur()==1)
+		{
+			delete this;
+			return expGauche;
+		}
 	}
+	return this;
+}
+
+void ExpAdd::afficher()
+{
+	gauche->afficher();
+	cout << " / ";
+	droite->afficher();
+}
